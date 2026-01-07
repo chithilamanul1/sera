bd7bd64ebbccddf1a44a8456b5c7908e30e02fea
chithila Manul <chithilamanul1@gmail.com>
2026-01-08 00:47:21 +0530

    Fix build: Resolve postcss conflict and restore shared Footer

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-}
-=======
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
->>>>>>> 65ba74e755105b740ed3fdbb4c8d0b96f251889d
